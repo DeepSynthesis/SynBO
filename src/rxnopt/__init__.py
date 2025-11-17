97b6676a081cdafb75da66c6b62fae1310a4f370
BoeingART <tzz24@mails.tsinghua.edu.cn>
2025-11-17 11:57:00 +0800

    update my name and email

--- src/rxnopt/__init__.py
+++ src/rxnopt/__init__.py
@@ -7,8 +7,8 @@ Bayesian Optimization with modern Python practices and rich output.
 from __future__ import annotations
 
 __version__ = "0.1.0"
-__author__ = "Luo Group"
-__email__ = "tzz24@mails.tsinghua.edu.cn"
+__author__ = "Zhenzhi Tan"
+__email__ = "zhenzhi-tan@outlook.com"
 
 # Core classes
 from .rxnopt import ReactionOptimizer
---
+++ src/rxnopt/utils/__init__.py
@@ -0,0 +1,8 @@
+# Core classes
+from .load_data import load_desc_dict, load_condition_dict, load_prev_rxn
+
+__all__ = [
+    "load_desc_dict",
+    "load_condition_dict",
+    "load_prev_rxn",
+]

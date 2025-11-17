d202fb2ca2ff643de28333bff045740860147abb
BoeingART <tzz24@mails.tsinghua.edu.cn>
2025-11-17 19:31:17 +0800

    update pipeline test for B-H dataset

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,3 +1,14 @@
 from rxnopt import ReactionOptimizer
+from rxnopt.utils import load_desc_dict
 
-rxn_opt = ReactionOptimizer()
+reagent_types = ["base", "ligand", "solvent"]
+index_col = [f"{r}_file_name" for r in reagent_types]
+
+rxn_opt = ReactionOptimizer(opt_metrics=["yield", "cost"])
+desc_dict, condition_dict = load_desc_dict(
+    reagent_types=reagent_types, desc_dir="dataset/descriptors", name_suffix="_dft", return_condition_dict=True, index_col=index_col
+)
+rxn_opt.load_rxn_space(condition_dict=condition_dict)
+rxn_opt.load_desc(desc_dict=desc_dict)
+rxn_opt.run()
+rxn_opt.save_results(save_dir="results")

938ddd4f006db46681511db5314e2d96ba539a49
BoeingART <tzz24@mails.tsinghua.edu.cn>
2025-12-09 20:15:10 +0800

    update dataset

--- examples/suzuki-science/cleanup.py
+++
@@ -1,7 +0,0 @@
-from quanda.utils import canonicalize_input_SMILES_file
-
-# canonicalize_input_SMILES_file("suzuki-HTE.csv", "base")
-import pandas as pd
-
-df = pd.read_csv("suzuki-HTE.csv")
-print(df["base"].drop_duplicates())

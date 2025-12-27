cf43547ee461675ac36bef28f0c5989def6a4b13
BoeingART <tzz24@mails.tsinghua.edu.cn>
2025-12-27 18:19:21 +0800

    also modified

--- src/rxnopt/bo_algorithm/bo_core.py
+++ src/rxnopt/bo_algorithm/bo_core.py
@@ -1,7 +1,7 @@
-from typing import List
+from typing import List, Tuple
 import numpy as np
 import torch
-from rich.progress import Progress
+from rich.progress import Progress, TextColumn, BarColumn, MofNCompleteColumn, TimeRemainingColumn
 
 from botorch.models import ModelListGP
 from botorch.utils.multi_objective.box_decompositions import NondominatedPartitioning
@@ -18,83 +18,110 @@ warnings.filterwarnings("ignore", category=NumericalWarning)
 
 
 class DefaultBO:
-    def __init__(self, mc_num_samples: int = 64, max_batch_size: int = 128, random_seed: int = 42):
+    def __init__(self, mc_num_samples: int = 64, max_batch_size: int = 128, random_seed: int = 42, opt_console=None):
         self.mc_num_samples = mc_num_samples
         self.random_seed = random_seed
         self.max_batch_size = max_batch_size
         self.surrogate_model_class = GPSurrogateModel
         self.acquisition_function_class = EHVIAcquisitionFunction
         self.target_evaluator = ParetoFrontCalculator()
+        self.opt_console = opt_console
 
     def optimize(
         self,
-        training_X_t: torch.Tensor,
-        training_y_t: torch.Tensor,
-        candidate_X_t: torch.Tensor,
+        training_X: np.ndarray,
+        training_y: np.ndarray,
+        candidate_X: np.ndarray,
         opt_metric_setting: List[dict],
         device: torch.device,
         batch_size: int,
-        progress: Progress,
-        task_train,
-        task_pareto,
-        task_acq_opt,
         training_y_dict: dict,
-    ):
-        models = []
-        num_models = training_y_t.shape[1]
-        for i in range(num_models):
-            key = list(training_y_dict.keys())[i]
-            progress.log(f"Fitting model for [bold]{key}[/bold]...", style="yellow")
+    ) -> Tuple[np.ndarray, List[str], np.ndarray, np.ndarray]:
+        training_X_t = torch.tensor(training_X).double().to(device=device)
+        training_y_t = torch.tensor(training_y).double()
+        training_y_t = self._weight_y(training_y_t, opt_metric_setting).to(device=device)
+        candidate_X_t = torch.tensor(candidate_X).double().to(device=device)
 
-            train_y_i = training_y_t[:, i].reshape(-1, 1)
-            model_i = self.surrogate_model_class(device=device, num_dims=training_X_t.shape[1])
-            model_i.fit(training_X_t, train_y_i)
-            models.append(model_i.model)
-            progress.update(task_train, advance=1)
+        with Progress(
+            TextColumn("[bold cyan]{task.description}"),
+            BarColumn(bar_width=None),
+            MofNCompleteColumn(),
+            TimeRemainingColumn(),
+            console=self.opt_console,
+        ) as progress:
+            num_models = training_y_t.shape[1]
+            task_train = progress.add_task(description="Training surrogate models", total=num_models, start=True)
+            
+            models = []
+            for i in range(num_models):
+                key = list(training_y_dict.keys())[i]
+                progress.log(f"Fitting model for [bold]{key}[/bold]...", style="yellow")
 
-        self.global_model = ModelListGP(*models)
+                train_y_i = training_y_t[:, i].reshape(-1, 1)
+                model_i = self.surrogate_model_class(device=device, num_dims=training_X_t.shape[1])
+                model_i.fit(training_X_t, train_y_i)
+                models.append(model_i.model)
+                progress.update(task_train, advance=1)
 
-        training_y_np = training_y_t.cpu().numpy()
-        self.pareto_y = self.target_evaluator.calculate_target_function(training_y_np, progress, task_pareto).to(device=device)
+            self.global_model = ModelListGP(*models)
 
-        self.ref_point = torch.tensor([-1 if omi["opt_direct"] == "min" else 0 for omi in opt_metric_setting], dtype=float, device=device)
+            task_pareto = progress.add_task(description="Calculating Pareto frontiers", total=len(training_y) - 1)
+            training_y_np = training_y_t.cpu().numpy()
+            self.pareto_y = self.target_evaluator.calculate_target_function(training_y_np, progress, task_pareto).to(device=device)
 
-        sampler = SobolQMCNormalSampler(sample_shape=torch.Size([self.mc_num_samples]), seed=self.random_seed)
-        partitioning = NondominatedPartitioning(ref_point=self.ref_point, Y=self.pareto_y)
-        acq_func = self.acquisition_function_class(
-            model=self.global_model,
-            sampler=sampler,
-            ref_point=self.ref_point,
-            partitioning=partitioning,
-        )
+            self.ref_point = torch.tensor([-1 if omi["opt_direct"] == "min" else 0 for omi in opt_metric_setting], dtype=float, device=device)
 
-        self.acq_result, self.acq_value = optimize_acqf_discrete(
-            acq_function=acq_func.ehvi,
-            choices=candidate_X_t,
-            q=batch_size,
-            max_batch_size=self.max_batch_size,
-            unique=True,
-            exclude_points=training_X_t,
-            min_distance=1e-6,
-            progress=progress,
-            task=task_acq_opt,
-        )
+            sampler = SobolQMCNormalSampler(sample_shape=torch.Size([self.mc_num_samples]), seed=self.random_seed)
+            partitioning = NondominatedPartitioning(ref_point=self.ref_point, Y=self.pareto_y)
+            acq_func = self.acquisition_function_class(
+                model=self.global_model,
+                sampler=sampler,
+                ref_point=self.ref_point,
+                partitioning=partitioning,
+            )
 
-        return self.acq_result, self.acq_value
+            task_acq_opt = progress.add_task(description="Optimizing acquisition function", total=batch_size)
+            self.acq_result, self.acq_value = optimize_acqf_discrete(
+                acq_function=acq_func.ehvi,
+                choices=candidate_X_t,
+                q=batch_size,
+                max_batch_size=self.max_batch_size,
+                unique=True,
+                exclude_points=training_X_t,
+                min_distance=1e-6,
+                progress=progress,
+                task=task_acq_opt,
+            )
 
-    def get_exploit_or_explore(self, acq_value, opt_console):
+        if device.type == "cuda":
+            best_samples = [res.cpu().numpy() for res in self.acq_result]
+        else:
+            best_samples = [res.numpy() for res in self.acq_result]
+
+        recommend_type = self._get_exploit_or_explore()
+        pred_mean, pred_std = self._get_predictions()
+
+        for i, d in enumerate(opt_metric_setting):
+            if d["opt_direct"] == "min":
+                pred_mean[:, i] = -pred_mean[:, i]
+
+        pred_mean = self._unweight_y(torch.tensor(pred_mean), opt_metric_setting).numpy()
+        pred_std = self._unweight_y(torch.tensor(pred_std), opt_metric_setting).numpy()
+
+        return best_samples, recommend_type, pred_mean, pred_std
+
+    def _get_exploit_or_explore(self) -> List[str]:
         with torch.no_grad():
             posterior = self.global_model.posterior(self.acq_result)
             pred_mean = posterior.mean
-            pred_var = posterior.variance
 
         hvi_values = torch.tensor([compute_hvi(pred_mean[i], self.pareto_y, self.ref_point) for i in range(pred_mean.shape[0])])
-        ehvi_values = acq_value.to(device="cpu")
+        ehvi_values = self.acq_value.to(device="cpu")
         exploit_scores = hvi_values / (ehvi_values + 1e-6)
         explore_scores = 1 - exploit_scores
 
         for i in range(self.acq_result.shape[0]):
-            opt_console.log(
+            self.opt_console.log(
                 f"Point {i}: "
                 f"EHVI = {ehvi_values[i]:.3f}, "
                 f"HVI = {hvi_values[i]:.3f}, "
@@ -103,10 +130,20 @@ class DefaultBO:
             )
         return ["exploit" if exploit_scores[i] > explore_scores[i] else "explore" for i in range(self.acq_result.shape[0])]
 
-    def get_predictions(self):
+    def _get_predictions(self) -> Tuple[np.ndarray, np.ndarray]:
         with torch.no_grad():
             posterior = self.global_model.posterior(self.acq_result)
             pred_mean = posterior.mean.cpu().numpy()
             pred_var = posterior.variance.cpu().numpy()
             pred_std = np.sqrt(pred_var)
         return pred_mean, pred_std
+
+    def _weight_y(self, training_y: torch.Tensor, opt_metric_setting: List[dict]) -> torch.Tensor:
+        weights = torch.tensor([d["metric_weight"] for d in opt_metric_setting])
+        training_y = training_y * weights
+        return training_y
+
+    def _unweight_y(self, training_y: torch.Tensor, opt_metric_setting: List[dict]) -> torch.Tensor:
+        weights = torch.tensor([d["metric_weight"] for d in opt_metric_setting])
+        training_y = training_y / weights
+        return training_y
--- src/rxnopt/optimize.py
+++ src/rxnopt/optimize.py
@@ -1,7 +1,6 @@
 from typing import List
 import numpy as np
 import torch
-from rich.progress import Progress, TextColumn, BarColumn, MofNCompleteColumn, TimeRemainingColumn
 
 from rxnopt.utils.logger import console
 from rxnopt.bo_algorithm.bo_core import DefaultBO
@@ -14,7 +13,7 @@ class Optimizer:
         self.opt_console = console
 
         if self.method == "default_BO":
-            self.optimizer = DefaultBO(mc_num_samples=mc_num_samples, max_batch_size=max_batch_size, random_seed=random_seed)
+            self.optimizer = DefaultBO(mc_num_samples=mc_num_samples, max_batch_size=max_batch_size, random_seed=random_seed, opt_console=console)
         else:
             raise Exception(f"Unknown optimization method: {self.method}")
 
@@ -35,78 +34,22 @@ class Optimizer:
         if isinstance(training_y, dict):
             training_y = np.array(list(training_y.values())).T
 
-        if self.method == "newBO":
-            raise Exception("newBO method has not been implemented yet.")
-        elif self.method == "random":
-            raise Exception("random method has not been implemented yet.")
-        elif self.method == "default_BO":
-            training_X_t = torch.tensor(training_X).double()
-            training_y_t = torch.tensor(training_y).double()
-            training_y_t = self._weight_y(training_y_t, opt_metric_setting)
-            candidate_X_t = torch.tensor(candidate_X).double()
-            dtype = torch.double
-            training_X_t = training_X_t.to(device=device, dtype=dtype)
-            training_y_t = training_y_t.to(device=device, dtype=dtype)
-            candidate_X_t = candidate_X_t.to(device=device, dtype=dtype)
-
-            with Progress(
-                TextColumn("[bold cyan]{task.description}"),
-                BarColumn(bar_width=None),
-                MofNCompleteColumn(),
-                TimeRemainingColumn(),
-                console=self.opt_console,
-            ) as progress:
-                num_models = training_y_t.shape[1]
-                task_train = progress.add_task(description="Training surrogate models", total=num_models, start=True)
-                task_pareto = progress.add_task(description="Calculating Pareto frontiers", total=len(training_y) - 1)
-                task_acq_opt = progress.add_task(description="Optimizing acquisition function", total=batch_size)
-
-                acq_result, acq_value = self.optimizer.optimize(
-                    training_X_t=training_X_t,
-                    training_y_t=training_y_t,
-                    candidate_X_t=candidate_X_t,
-                    opt_metric_setting=opt_metric_setting,
-                    device=device,
-                    batch_size=batch_size,
-                    progress=progress,
-                    task_train=task_train,
-                    task_pareto=task_pareto,
-                    task_acq_opt=task_acq_opt,
-                    training_y_dict=training_y_dict,
-                )
-
-            if device.type == "cuda":
-                best_samples = [res.cpu().numpy() for res in acq_result]
-            else:
-                best_samples = [res.numpy() for res in acq_result]
-
-            recommend_type = self.optimizer.get_exploit_or_explore(acq_value, self.opt_console)
+        if self.method == "default_BO":
+            best_samples, recommend_type, pred_mean, pred_std = self.optimizer.optimize(
+                training_X=training_X,
+                training_y=training_y,
+                candidate_X=candidate_X,
+                opt_metric_setting=opt_metric_setting,
+                device=device,
+                batch_size=batch_size,
+                training_y_dict=training_y_dict,
+            )
 
             selected_indices = [np.argwhere(np.all(candidate_X == best_sample, axis=1)).flatten() for best_sample in best_samples]
             selected_indices = np.array(selected_indices).squeeze()
             selected_conditions = self.name_data[selected_indices].squeeze()
 
-            pred_mean, pred_std = self.optimizer.get_predictions()
-
-            for i, d in enumerate(opt_metric_setting):
-                if d["opt_direct"] == "min":
-                    pred_mean[:, i] = -pred_mean[:, i]
-
-            pred_mean = self._unweight_y(torch.tensor(pred_mean), opt_metric_setting).numpy()
-            pred_std = self._unweight_y(torch.tensor(pred_std), opt_metric_setting).numpy()
-
             self.opt_console.print("✅ Finish optimization", style="green")
             return selected_conditions, recommend_type, pred_mean, pred_std
         else:
             raise Exception(f"Unknown optimization method: {self.method}")
-
-    def _weight_y(self, training_y: torch.Tensor, opt_metric_setting: List[dict]) -> torch.Tensor:
-        weights = torch.tensor([d["metric_weight"] for d in opt_metric_setting])
-
-        training_y = training_y * weights
-        return training_y
-
-    def _unweight_y(self, training_y: torch.Tensor, opt_metric_setting: List[dict]) -> torch.Tensor:
-        weights = torch.tensor([d["metric_weight"] for d in opt_metric_setting])
-        training_y = training_y / weights
-        return training_y

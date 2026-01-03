1bb05ed2101191b6b67ca1daf2fa687179d4914d
BoeingART <tzz24@mails.tsinghua.edu.cn>
2026-01-03 16:03:27 +0800

    update check_dataset part.

--- examples/check_dataset.py
+++ examples/check_dataset.py
@@ -1,9 +1,35 @@
 import pandas as pd
 from itertools import product
 
+dataset_info = {
+    "asym-hydrogenation": {
+        "file_path": "asym-hydrogenation/asym_hydrogenation.csv",
+        "reagent_columns": [
+            "reagent",
+            "solvent",
+            "ligand",
+            "metal_amount",
+            "ligand_amount",
+            "temperature",
+            "pressure",
+            "time",
+        ],
+    },
+    "1430-UHTE": {
+        "file_path": "1430-ultra-HTE/1430-Ultra-high-throughput.csv",
+        "reagent_columns": ["Reaction1", "Reaction2", "Catalyst1", "Catalyst2"],
+    },
+    "B-H-HTE": {"file_path": "B-H-HTE/B-H-dataset.csv", "reagent_columns": ["base", "ligand", "solvent", "concentration", "temperature"]},
+    "suzuki": {
+        "file_path": "suzuki-science/suzuki-HTE.csv",
+        "reagent_columns": ["product", "solvent", "catalyst", "ligand", "reactant2", "reactant1", "base"],
+    },
+}
+
 
 def process_reagent_data(file_path, reagent_cols):
     df = pd.read_csv(file_path)
+    # df = df[df["reagent"] == "COC(=O)/C(=C/c1ccccc1)NC(C)=O"]
     reagent_list = []
     for col in reagent_cols:
         reagent_list.append(df[col].drop_duplicates())
@@ -19,8 +45,9 @@ def process_reagent_data(file_path, reagent_cols):
 
 
 # Example usage
-file_path = "1430-ultra-HTE/1430-Ultra-high-throughput.csv"
-reagent_cols = ["Reaction1", "Reaction2", "Catalyst1", "Catalyst2"]
+dataset = "suzuki"
+file_path = dataset_info[dataset]["file_path"]
+reagent_cols = dataset_info[dataset]["reagent_columns"]
 result_df, coverage, counts = process_reagent_data(file_path, reagent_cols)
 print(f"Coverage Ratio: {coverage}")
 print("Reagent Counts:")

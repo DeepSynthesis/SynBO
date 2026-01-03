af9776268c71870b48f0a01bec5a2592a192e11e
BoeingART <tzz24@mails.tsinghua.edu.cn>
2026-01-03 15:52:13 +0800

    update asym_hydrogenation part.

--- examples/canon_data.py
+++ examples/canon_data.py
@@ -1,5 +1,3 @@
 from quanda.utils import canonicalize_input_SMILES_file
 
-canonicalize_input_SMILES_file(
-    "1430-ultra-HTE/1430-Ultra-high-throughput.csv", ["Reaction1", "Reaction2", "Catalyst1", "Catalyst2", "Product"]
-)
+canonicalize_input_SMILES_file("asym-hydrogenation/asym_hydrogenation.csv", ["reagent", "solvent", "product"])

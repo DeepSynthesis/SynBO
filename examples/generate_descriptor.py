b6ada485f096c0d1397c67bd14a08e206fb16421
BoeingART <tzz24@mails.tsinghua.edu.cn>
2026-01-22 14:42:35 +0800

    update all results.

--- examples/generate_descriptor.py
+++ examples/generate_descriptor.py
@@ -9,4 +9,4 @@ df = pd.read_csv(Path(dataset_name) / Path(f"{dataset_name}.csv"))
 for mol_type in mol_types:
     smiles_list = df[mol_type].drop_duplicates()
     smiles_list = pd.read_csv(Path(dataset_name) / Path(f"descriptors/{mol_type}_dft.csv"))[f"{mol_type}_SMILES"]
-    calc_spoc_desc(smiles_list=smiles_list, save_path=Path(dataset_name) / Path(f"descriptors/{mol_type}.csv"), fp_type="RDKit")
+    calc_spoc_desc(smiles_list=smiles_list, save_path=Path(dataset_name) / Path(f"descriptors/{mol_type}.csv"), fp_type="OneHot")
